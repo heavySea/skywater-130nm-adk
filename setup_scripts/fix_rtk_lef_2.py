3b14896af0d7209ab987d0ce1118dacd04cf9777
maximilian.koschay <mkoschay@web.de>
2021-06-08 17:59:49 +0200

    Small tech LEF update

--- setup_scripts/fix_rtk_lef_2.py
+++ setup_scripts/fix_rtk_lef_2.py
@@ -1,4 +1,4 @@
-2# Hacky LEF Edits Part 2
+# Hacky LEF Edits Part 2
 #
 # "With the current version of the lef file, Innovus gives an error: **ERROR: (IMPLF-121):   
 #  You need to have cut layer after layer 'pwell'.. Manually add the licon layer after pwell layer definition in rtk-tech.lef. 
@@ -11,7 +11,17 @@ from shutil import copyfile
 
 print ("Fixing tech LEF file (part 2). Read the docs about this step to check if these modifications are still relevant!")
 
-insert_text="LAYER licon \n  TYPE CUT ;\nEND licon"
+#insert_text="LAYER licon \n  TYPE CUT ;\nEND licon"
+
+insert_text	= "### https://github.com/google/skywater-pdk-libs-sky130_fd_sc_hd/pull/3/files\n"
+			+ "LAYER poly\n    TYPE MASTERSLICE ;\nEND poly"
+			+ "LAYER licon1\n    TYPE CUT ;\n"
+			+ "    WIDTH 0.17 ;                # Licon 1\n"
+			+ "    SPACING 0.17 ;              # Licon 2\n"
+			+ "    ENCLOSURE BELOW 0 0 ;       # Licon 4\n"
+			+ "    ENCLOSURE ABOVE 0.08 0.08 ; # Poly / Met1 4 / Met1 5\n"
+			+ "END licon1\n###"
+
 file_insert_line_at_pattern( 'rtk-tech.lef', 'END pwell', insert_text, replace_line=False)
 
 # Copy final RTK LEF into standard view
